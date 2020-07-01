7c5dc1c2a45da0021244531d161e47d8b92d12ee
Simen <simen@eide.ai>
2020-07-01 19:00:03 +0200

    steer instead of reverse, lower th

--- models.py
+++ models.py
@@ -4,8 +4,8 @@ import torch.nn as nn
 class GreenNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.crop_y_min = 150
-        self.th=0.2
+        self.crop_y_min = 100
+        self.th=0.1
         self.fc = nn.Linear(1,4)
     def forward(self, img):
         bottom_half_col = img[:,:,self.crop_y_min:,:].mean(2).mean(2)
@@ -14,6 +14,6 @@ class GreenNet(nn.Module):
 
         out = torch.zeros((len(img), 4))
         out[(green>self.th),0] = 1.0
-        out[(green<=self.th),1] = 1.0
+        out[(green<=self.th),2] = 1.0
         #out = self.fc(bottom_half_green)
         return out
