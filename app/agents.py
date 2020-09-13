5ab9e91b35344559e75e0c5af15bf1bc75af42c3
simen@goat <simen@goat>
2020-09-13 10:18:23 +0000

    make option agent instead for better overview

--- app/agents.py
+++ app/agents.py
@@ -19,35 +19,7 @@ class GoatAgent:
         self.agent_ai = models.GreenNet()
         
         self.mode = "human"
-
-        # Backandturn parameters:
-        self.starttime_backandturn = False
-        self.back_time = None
-        self.rot_time = None
-
-    def start_backandturn(self, back_time=None, rot_time=None):
-        
-        self.starttime_backandturn = datetime.datetime.now()
-        if back_time is None:
-            self.back_time = 2 #random.randint(1,3)
-        if rot_time is None:
-            self.rot_time = random.randint(2,4)
-        logging.info(f"Starting backandturn manouvre..: back={self.back_time}, turn={self.rot_time}")
-        return self.action_backandturn()
-    
-    def action_backandturn(self):
-        """ A special move that override other moves once started. """
-        time_since_started = (datetime.datetime.now()-self.starttime_backandturn).seconds
-        if time_since_started < self.back_time:
-            return {'left' : -1, 'right' : -1, 'action' : "backandturn"}
-        elif time_since_started < (self.back_time + self.rot_time):
-            return {'left' : -1, 'right' : 1, 'action' : "backandturn"}
-        else:
-            logging.info("Done with backandturn move.")
-            self.starttime_backandturn=False
-            if self.mode == "human":
-                self.agent_ui.key=None
-            return {'left' : 0, 'right' : 0}
+        self.active_option = None
         
     def step(self, *args, **kwargs):
         """
@@ -59,24 +31,59 @@ class GoatAgent:
         action = self.agent_ui(*args, **kwargs)
 
         
-        # Execute UI if left or right is given:
+        # Execute UI straight away if actions are given:
         if (action.get("action") not in ['AI','backandturn']):
             self.mode = "human" #cancel any other mode
+            self.active_option = None
             self.starttime_backandturn = False
             action['mode'] = self.mode
+            if self.active_option:
+                action['active_option'] = self.active_option.name
             return action
-        
-        # If we are in backturn move do that:
-        if self.starttime_backandturn:
-            action = self.action_backandturn()
         elif action.get("action") == "AI":
             self.mode = "AI"
+        
+        if self.active_option:
+            action, done = self.active_option()
+            print(done)
+            if done:
+                self.active_option=None
+                if self.mode == "human":
+                    self.agent_ui.key=None
+                
+        elif self.mode =="AI":
             action = self.agent_ai.step(*args, **kwargs)
+
+        if (action.get("action") == "backandturn") & (not self.active_option):
+            self.active_option = BackAndTurn()
             
-        if (action.get("action") == "backandturn") & (self.starttime_backandturn is False):
-            action = self.start_backandturn()
         
         action['mode'] = self.mode
+        if self.active_option:
+            action['active_option'] = self.active_option.name
         return action
 
+class BackAndTurn:
+    def __init__(self, back_time=None, rot_time=None):
+        # Backandturn parameters:
+        self.name = "BackAndTurn"
+        self.starttime_backandturn = datetime.datetime.now()
+        self.back_time = None
+        self.rot_time = None
         
+        if back_time is None:
+            self.back_time = 2
+        if rot_time is None:
+            self.rot_time = random.randint(2,4)
+        logging.info(f"Starting backandturn manouvre..: back={self.back_time}, turn={self.rot_time}")
+
+    def __call__(self):
+        """ A special move that override other moves once started. """
+        time_since_started = (datetime.datetime.now()-self.starttime_backandturn).seconds
+        if time_since_started < self.back_time:
+            return {'left' : -1, 'right' : -1, 'action' : "backandturn"}, False
+        elif time_since_started < (self.back_time + self.rot_time):
+            return {'left' : -1, 'right' : 1, 'action' : "backandturn"}, False
+        else:
+            logging.info("Done with backandturn move.")
+            return {'left' : 0, 'right' : 0}, True
