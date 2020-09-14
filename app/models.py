7f3cc1114f33602c42475b6cdc23573a3cd0bb92
simen@colab <simen@colab>
2020-09-14 13:02:16 +0000

    running training script

--- app/models.py
+++ app/models.py
@@ -4,13 +4,57 @@ import random
 import datetime
 import logging
 import time
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, models, transforms
+import pickle
+import cv2
+import PIL
+### SUPPORTED ACTIONS
+idx2action = {
+    0 : '<UNK>',
+    1 : 'stop',
+    2 : 'backandturn',
+    3 : 'forward',
+    }
+action2idx = {val : key for key, val in idx2action.items()}
+
+### TRANSFORMS
+mean_nums = [0.485, 0.456, 0.406]
+std_nums = [0.229, 0.224, 0.225]
+
+tr = transforms.Compose([
+        transforms.RandomResizedCrop(size=256),
+        #transforms.RandomRotation(degrees=15),
+        #transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean_nums, std_nums)
+])
+
+### DATALOADERS ###
+class GardenData(torch.utils.data.Dataset):
+    def __init__(self, dat, device="cpu"):
+        self.dat = dat
+        self.device = device
+
+    def __getitem__(self, idx):
+        row = self.dat.iloc[idx]
+
+        out = {
+            'image' : tr(PIL.Image.open(row.image_path)),
+            'action' : action2idx.get(row.action,0)}
+        return out
+    def __len__(self):
+        return len(self.dat)
+
+
 class GardenNet(nn.Module):
     def __init__(self):
         super().__init__()
-        self.idx2action = {0 : 'stop',
-                          1 : 'backandturn',
-                          2 : 'forward',
-                          }
+        self.idx2action = idx2action
         self.num_actions = len(self.idx2action)
         
     @torch.no_grad()
@@ -18,7 +62,7 @@ class GardenNet(nn.Module):
         if state is None:
             return {}
         else:
-            img = torch.tensor(state['camera']/255.).unsqueeze(0)
+            img = tr(state['camera'])
             actionvec = self.forward(img)
 
             action = self.idx2action[actionvec.argmax().item()]
@@ -30,6 +74,32 @@ class GardenNet(nn.Module):
             out['action'] = action
             return out
         
+class ConvNet(GardenNet):
+    def __init__(self, input_size=256):
+        super().__init__()
+        self.input_size = input_size
+        self.channels = [3,16,32,64]
+        self.layers = nn.ModuleList()
+        for i in range(1,len(self.channels)):
+            self.layers.append(
+                nn.Conv2d(self.channels[i-1],self.channels[i], kernel_size=5, stride=2)
+                )
+
+            self.layers.append(
+                nn.MaxPool2d(3, stride=2)
+            )
+            
+        self.fc = nn.Linear(256, self.num_actions)
+
+    def forward(self, img):
+        x = img
+        for layer in self.layers:
+            x = layer(x)
+            #print(x.size())
+        x = x.flatten(1)
+        #print(x.size())
+        x = self.fc(x)
+        return x
         
 
 class GreenNet(GardenNet):
@@ -37,13 +107,13 @@ class GreenNet(GardenNet):
         super().__init__()
         self.crop_y_min = 100
         self.th=0.1
-        self.fc = nn.Linear(1,4)
+        self.fc = nn.Linear(1,len(idx2action))
 
         
     def forward(self, img):
         bottom_half_col = img[:,:,self.crop_y_min:,:].mean(2).mean(2)
         bottom_half_col.size()
-        bottom_half_col = bottom_half_col- bottom_half_col.mean(1)
+        bottom_half_col = bottom_half_col- bottom_half_col.mean(1).unsqueeze(1)
         green = bottom_half_col[:,1]
         blue = bottom_half_col[:,2]
         
@@ -55,4 +125,14 @@ class GreenNet(GardenNet):
         out[:,2]= forward
         return out
     
-    
+class ColourNet(GardenNet):
+    def __init__(self, load_parameters=False):
+        super().__init__()
+        self.crop_y_min = 200
+        self.fc = nn.Linear(3,len(idx2action))
+
+        
+    def forward(self, img):
+        bottom_half_col = img[:,:,self.crop_y_min:,:].mean(2).mean(2)
+        out = self.fc(bottom_half_col)
+        return out
--- app/train.py
+++ app/train.py
@@ -1,8 +1,21 @@
 #%%
 import os
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.optim import lr_scheduler
+import torchvision
+from torchvision import datasets, transforms
+import pandas as pd
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s', level='INFO')
+import pickle
+import matplotlib.pyplot as plt
+import cv2
+#%%
 tubs = os.listdir("data")
 
-episode = max(tubs)
+episode = 'tub_2020-09-14.10:47:48' #max(tubs)
 data_dir = f"data/{episode}"
 files = os.listdir(data_dir)
 files.sort()
@@ -10,13 +23,10 @@ files.sort()
 event_files = [f for f in files if "event" in f]
 
 #%%
-import pandas as pd
-import logging
-import pickle
+
 def loader(path):
     with open(path, "rb") as handle:
         dat = pickle.load(handle)
-
     return dat
 
 L = []
@@ -29,44 +39,30 @@ for filename in event_files:
         logging.warning(f"Failed to load {path}")
 #%%
 dat = pd.DataFrame(L)
+logging.info(f"Loaded {len(dat)} events into dataframe.")
 dat.head()
-len(dat)
-# extract_actions
-dat['throttle'] = dat.action.map(lambda a: a['throttle'])
-dat['steer'] = dat.action.map(lambda a: a['steer'])
-
-dat = dat.dropna().reset_index(drop=True)
-dat.head()
-#plt.imshow(dat['image'][0])
+#dat['image_path'] = dat.image_path.map(lambda s: f"/root/{s}")
 #%%
-import matplotlib.pyplot as plt
-plt.imshow(loader(dat.image_path[1]))
 
+# extract_actions
+dat['action_dict'] = dat['action']
+dat['action'] = dat.action.map(lambda a: a.get('action'))
+dat = dat.dropna().sort_values("step").reset_index(drop=True)
+dat.head()
+#%% LOAD IMAGE TO TEST
+#plt.imshow(cv2.imread(dat.image_path[1]))
 
-# %%
-import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.optim import lr_scheduler
-import torchvision
-from torchvision import datasets, models, transforms
-# %%
+#%%
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-import model_drive
+import models
 # %%
-dataset = model_drive.GardenData(dat)
-dl = torch.utils.data.DataLoader(dataset=dataset, batch_size = 16)
+dataset = models.GardenData(dat, device="cuda")
+dl = torch.utils.data.DataLoader(dataset=dataset, batch_size = 32, num_workers=0)
 dataloaders = {'train' : dl}
 dataset_sizes = {name : len(dl.dataset) for name, dl in dataloaders.items()}
-batch = next(iter(dl))
-#%% TRANSFORM
-
-#%% BUILD MODEL
+#%%
 from torch import nn
-model = models.resnet34(pretrained=True)
-num_ftrs = model.fc.in_features
-model.fc = nn.Linear(num_ftrs, 4)
+model = models.ConvNet()
 model = model.to(device)
 criterion = nn.CrossEntropyLoss()
 
@@ -74,80 +70,28 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 scheduler = lr_scheduler.StepLR(optimizer, step_size=7, gamma=0.1)
 
-# %%
-def train_model(model, criterion, optimizer, scheduler, num_epochs=10):
-    since = time.time()
-
-    best_model_wts = copy.deepcopy(model.state_dict())
-    best_acc = 0.0
-
-    for epoch in range(num_epochs):
-        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
-        print('-' * 10)
-
-        # Each epoch has a training and validation phase
-        for phase in ['train']:
-            if phase == 'train':
-                scheduler.step()
-                model.train()  # Set model to training mode
-            else:
-                model.eval()   # Set model to evaluate mode
-
-            current_loss = 0.0
-            current_corrects = 0
-
-            # Here's where the training happens
-            print('Iterating through data...')
-
-            for batch in dataloaders[phase]:
-                inputs = batch['image'].to(device)
-                labels = batch['label'].to(device)
-
-                # We need to zero the gradients, don't forget it
-                optimizer.zero_grad()
-
-                # Time to carry out the forward training poss
-                # We only need to log the loss stats if we are in training phase
-                with torch.set_grad_enabled(phase == 'train'):
-                    outputs = model(inputs)
-                    _, preds = torch.max(outputs, 1)
-                    loss = criterion(outputs, labels)
-
-                    # backward + optimize only if in training phase
-                    if phase == 'train':
-                        loss.backward()
-                        optimizer.step()
-
-                # We want variables to hold the loss statistics
-                current_loss += loss.item() * inputs.size(0)
-                current_corrects += torch.sum(preds == labels.data)
-
-            epoch_loss = current_loss / dataset_sizes[phase]
-            epoch_acc = current_corrects.double() / dataset_sizes[phase]
-
-            print('{} Loss: {:.4f} Acc: {:.4f}'.format(
-                phase, epoch_loss, epoch_acc))
-
-            # Make a copy of the model if the accuracy on the validation set has improved
-            if phase == 'val' and epoch_acc > best_acc:
-                best_acc = epoch_acc
-                best_model_wts = copy.deepcopy(model.state_dict())
-
-        print()
-
-    time_since = time.time() - since
-    print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_since // 60, time_since % 60))
-    print('Best val Acc: {:4f}'.format(best_acc))
-
-    # Now we'll load in the best model weights and return it
-    model.load_state_dict(best_model_wts)
-    return model
-
+dataloaders['train'].dataset.dat['action'].hist()
+#%%
+num_epochs=100
+for ep in range(num_epochs):
+    phase = "train"
+    stats = {'num_obs' : 0, 'loss' : 0, 'corrects' : 0}
+    for batch in dataloaders[phase]:
+        batch = {key : val.to(device) for key, val in batch.items()}
+        optimizer.zero_grad()
+        yhat = model(batch['image'])
+        loss = criterion(yhat, batch['action'])
+        if phase == 'train':
+            loss.backward()
+            optimizer.step()
+        with torch.no_grad():
+            
+            pred_class = torch.argmax(yhat,1)
+            stats['loss'] += loss
+            stats['corrects'] += (pred_class == batch['action']).sum().float()
+            stats['num_obs'] += len(batch['action'])
+    logging.info(f"Episode {ep}: loss={stats['loss']/stats['num_obs']:.3f}, accuracy={stats['corrects']/stats['num_obs']:.2f}")
 # %%
 import time
-import copy
-train_model(model = model, criterion = criterion, optimizer =optimizer, scheduler=scheduler, num_epochs=1)
-
 torch.save(model, "model.pickle")
 # %%
